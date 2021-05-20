4bc936a7f09a5d4062407e884243b36955add04d
Angelika Haftarczyk <angelika.haftarczyk@gmail.com>
2021-05-20 22:51:26 +0200

    Dodanie listowania

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,5 +1,7 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,13 +11,43 @@ public class TaskManager {
     static String[][] tasks;
 
     public static void main(String[] args) {
+
+        tasks = readCsv("tasks.csv");
+        printOption();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while(!input.equals("exit")) {
+
+            switch (input) {
+                case "add":
+//                    addTask();
+                    System.out.println("add");
+                    break;
+                case "remove":
+                     removeTask();
+                    System.out.println("remove");
+                    break;
+                case "list":
+                    listTask();
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+            printOption();
+            input = scanner.nextLine();
+
+        }
+
+
+
+    }
+    public static void printOption() {
         System.out.println(ConsoleColors.BLUE + "Please select an option: ");
         System.out.print(ConsoleColors.RESET);
         System.out.println("add" + "\nremove" + "\nlist" + "\nexit");
-
-        tasks = readCsv("tasks.csv");
-
-
     }
 
     public static String[][] readCsv(String fileName) {
@@ -40,4 +72,19 @@ public class TaskManager {
         }
         return result;
     }
+
+    public static void removeTask() {
+
+    }
+
+    public static void listTask(){
+        System.out.println("list");
+        for (int i = 0; i < tasks.length; i++) {
+            String[] task = tasks[i];
+            System.out.println(i+" : " + task[0] + " "+ task[1] + " " + task[2]);
+
+        }
+    }
+
+
 }
